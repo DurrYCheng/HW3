b5f349284914d9b3e92f7690f67bf6f5177343ec
DurrYCheng <lamert93389744@gmail.com>
2022-10-27 13:48:56 +0800

    done

--- 109360451_楊澄/5.36/source/Main.c
+++ 109360451_楊澄/5.36/source/Main.c
@@ -1,27 +1,27 @@
 # include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+int times = 0;
 
-void Towers(int n, char a, char b, char c) 
-{
-	if (n == 1) {
-		cout << "Move disk " << n << " from" << a << " to " << c << endl;
-	}
-	else {
-		Towers(n - 1, a, c, b);
-		cout << "Move disk " << n << " from" << a << " to " << c << endl;
-		Towers(n - 1, b, a, c);
-	}
-}
-int main(int argc, char* argv[]) {
-	int n;
-	cin >> n;
-	Towers(n, 'A', 'B', 'C');
-	cout << endl;
-
-
+void hanoi(int n, char A, char B, char C) {
+    if (n == 1) {
+        printf("從 %c 移到 %c\n", A, C);
+        times++;
+    }
+    else {
+        hanoi(n - 1, A, C, B);	//先將最底層以外的都移到B 
+        hanoi(1, A, B, C);		//再把底層移到C 
+        hanoi(n - 1, B, A, C);	//在花跟第一次一樣的次數，把B都移到C 
+    }
 }
 
-
+int main() {
+    int n;
+    printf("輸入河內塔層數：");
+    scanf("%d", &n);
+    hanoi(n, 'A', 'B', 'C');
+    printf("完成轉移需%d個步驟\n", times);
+    system("pause");
+    return 0;
+}
